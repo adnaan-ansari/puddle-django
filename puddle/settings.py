16a757646bd1cc01e60c2ad9781e893dc8f771d5
ravindranag <ravindranag52@gmail.com>
2024-06-16 01:20:25 +0530

    env

--- puddle/settings.py
+++ puddle/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", ".onrender.com"]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -75,22 +80,13 @@ TEMPLATES = [
     },
 ]
 
-application = get_wsgi_application()
-# add this vercel variable
-app = application
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+print("database url", os.environ.get('DATABASE_URL'))
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'puddle',
-        'USER': '<myuser>',
-        'PASSWORD': '<mypass>',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
